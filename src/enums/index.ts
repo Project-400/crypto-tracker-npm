a90a98585d61c570916dceb4b039a121da4caf49
Charles McCarthy <charles@studybundles.com>
2020-08-16 19:03:22 +0100

    Initial Setup

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,3 +1,6 @@
-export enum EmptyEnum {
-
+export enum TradingBotState {
+    WAITING, // Waiting to trade
+    TRADING, // Bot has bought currency and watching for profit / loss
+    PAUSED, // Sold currency due to drop in price - Holding out until price increases
+    FINISHED // Bot has exited currency trading due to loss or decreasing price
 }
--- src/interfaces/objects.ts
+++ src/interfaces/objects.ts
@@ -1,1 +1,29 @@
-export interface EmptyInterface { }
+import { TradingBotState } from '../enums';
+
+export interface CryptoTradingBot {
+    symbol: string; // Pair symbol, eg. BTCUSDT
+    quote: string; // Currency being used to buy base, eg. USDT
+    base: string; // Currency being bought, eg. BTC
+    spendLimit: number; // Limited amount of quote currency to use
+    isSpendLimited: boolean; // Is the bot limited to spendLimit
+    currentSpend: number; // Current amount of quote currency being used
+    profits: number; // Total profits / loss in quote currency
+    state: TradingBotState; // Current state of bot
+}
+
+export interface ExchangeInfoSymbol {
+    symbol: string;
+    status: string;
+    baseAsset: string;
+    baseAssetPrecision: number;
+    quoteAsset: string;
+    quotePrecision: number;
+    quoteAssetPrecision: number;
+    orderTypes: string[];
+    icebergAllowed: boolean;
+    ocoAllowed: boolean;
+    isSpotTradingAllowed: boolean;
+    isMarginTradingAllowed: boolean;
+    filters: any[];
+    permissions: string[];
+}
