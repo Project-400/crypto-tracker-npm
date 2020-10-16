74b7d7b489c5d5e27bf4b9413962cf78f71cee73
Charles McCarthy <charles@studybundles.com>
2020-10-16 17:07:10 +0100

    Transactions Interfaces Migrated

---
+++ src/interfaces/transactions.ts
@@ -0,0 +1,15 @@
+export interface BuyCurrencyData {
+    symbol: string;
+    base: string;
+    quote: string;
+    quantity: number;
+    isTest?: boolean;
+}
+
+export interface SellCurrencyData {
+    symbol: string;
+    base: string;
+    quote: string;
+    quantity: number;
+    isTest?: boolean;
+}
