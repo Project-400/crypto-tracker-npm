d661ff500230ce92a2a265ad8767509ea1a95795
Charles McCarthy <charles@studybundles.com>
2021-02-18 17:43:36 +0000

    Kline Values Object

--- lib/interfaces/wallet-valuation.d.ts
+++ lib/interfaces/wallet-valuation.d.ts
@@ -18,3 +18,19 @@ export interface WalletValue extends DBItem {
         valueEndingAt?: string;
     };
 }
+export interface KlineValues extends DBItem {
+    time: string;
+    open: string;
+    close?: string;
+    lowest: string;
+    highest: string;
+    average: string;
+    interval: VALUE_LOG_INTERVAL;
+    isClosed: boolean;
+    times: {
+        createdAt: string;
+        updatedAt?: string;
+        valueStartingAt?: string;
+        valueEndingAt?: string;
+    };
+}
